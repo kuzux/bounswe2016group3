d154a48f7677b86c21db0dbe4f1c42ff2cd342bb
Safa Andac <machinegunner.safa@gmail.com>
2016-05-10 01:44:57 +0300

    Experiment

---
+++ indv/Solution.java
@@ -0,0 +1,46 @@
+import org.apache.jena.query.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+/**
+ * Created by sand94 on 20.04.2016.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        String queryString = "PREFIX entity: <http://www.wikidata.org/entity/> " +
+                " SELECT ?predicate ?object " +
+                " WHERE { entity:Q23 ?predicate ?object . } ";
+        //" LIMIT 100 ";
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qexec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+        ResultSet results = qexec.execSelect();
+        while (results.hasNext()) {
+            QuerySolution binding = results.nextSolution();
+            //Datalari MySQL e atilacak...
+            System.out.println(binding.toString());
+        }
+
+        //
+        /*Connection conn = null;
+        PreparedStatement pst = null;
+        String url = "jdbc:mysql://localhost:3306/mysql";
+        String user = "root";
+        String password = "6pefwgb";
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection Done");
+        }
+        catch (SQLException ex){
+            System.out.println("ERROR!!!");
+        }
+        finally {
+            System.out.println("Sayounora...");
+        }*/
+    }
+}
+
